9891c204215fbf0981ae99fb2f04a662cd01ce20
AneliaGaydardzhieva <92336807+AneliaUCL@users.noreply.github.com>
2021-11-14 18:46:26 +0100

    Database initial setup

--- db_script.py
+++ db_script.py
@@ -8,33 +8,62 @@ con = sqlite3.connect('database.db')
 #with database
 cur = con.cursor()
 
-# Create table user table
-table1 = '''CREATE TABLE IF NOT EXISTS 
-user (user id INTEGER PRIMERY KEY, first_name, last_name, username, mobile, age, email, password)'''
+# CREATE TABLES
+users = '''CREATE TABLE IF NOT EXISTS 
+users (user_id INTEGER PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), username VARCHAR(255), mobile VARCHAR(255), age INTEGER, email VARCHAR(255), address VARCHAR(500), password VARCHAR(255), is_activated BOOL, is_admin BOOL)
+'''
 
-cur.execute(table1)
+admins = '''CREATE TABLE IF NOT EXISTS 
+admins (user_id INTEGER PRIMARY KEY)
+'''
+volunteers = '''CREATE TABLE IF NOT EXISTS 
+volunteers (user_id INTEGER PRIMARY KEY, camp_id INTEGER, availability VARCHAR(100), medical_history VARCHAR(1000))
+'''
 
-# Insert a row of data
-cur.execute("INSERT INTO user VALUES ('First', 'Last', 'username', 07383736474, 100, 'test_user@email.com', 'abcd1234')")
+emergency_plans = '''CREATE TABLE IF NOT EXISTS 
+emergency_plans (em_plan_id INTEGER PRIMARY KEY, camps_count, type, description, geo_area, start_date, end_date)
+'''
 
-# Save (commit) the changes
+camps = '''CREATE TABLE IF NOT EXISTS 
+camps (camp_id INTEGER PRIMARY KEY, em_plan_id, refugees_count INTEGER, food_rations_count INTEGER, shelter_count INTEGER, volunteers_count INTEGER, medics_count INTEGER)
+'''
+
+refugees = '''CREATE TABLE IF NOT EXISTS 
+refugees (refugee_id INTEGER PRIMARY KEY, camp_id, first_name VARCHAR(255), last_name VARCHAR(255), relative_name VARCHAR(255), medical_history VARCHAR(1000))'''
+
+
+
+cur.execute(users)
+cur.execute(admins)
+cur.execute(volunteers)
+cur.execute(emergency_plans)
+cur.execute(camps)
+cur.execute(refugees)
+
+
+#Save (commit) the changes
 con.commit()
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
+#We can also close the connection if we are done with it.
+#Just be sure any changes have been committed or they will be lost.
 con.close()
 
-#get results 
-cur.execure("SELECT * FROM user")
 
-results = cur.fetchall()
-print(results)
+#INSERT COMMAND (create a user, emergency plan, user, refugee, etc)
+# cur.execute("INSERT INTO user VALUES ('First', 'Last', 'username', 07383736474, 100, 'test_user@email.com', 'abcd1234')")
 
-#update
-cur.execute("UPDATE user SET first_name = 'FirstUpdated' WHERE user_id = 1")
 
-#delete
-cur.execute('DELETE FROM user WHERE user_id = 1')
+#SEARCH DATABASE COMMAND (admin searching for a refugee; emergency plans summaries)
+# cur.execure("SELECT * FROM user")
+
+# results = cur.fetchall()
+# print(results)
+
+#UPDATE TABLE COMMAND (volunteers editing their info and refugees info)
+# cur.execute("UPDATE user SET first_name = 'FirstUpdated' WHERE user_id = 1")
+
+#DELETE ENTRY COMMAND (delete volunteer/refugee etc.)
+# cur.execute('DELETE FROM user WHERE user_id = 1')
 
 
 
