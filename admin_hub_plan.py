c21b96e4b1f410236900bcaed415a24583d34450
mkiselev2002 <mkiselev2002@icloud.com>
2021-11-17 15:58:38 +0000

    solved import problems

--- admin_hub_plan.py
+++ admin_hub_plan.py
@@ -264,7 +264,7 @@ def search_plan_name(e):
                     plan_treeview.insert("", "end", values=row)
 
 
-def show_emergency_plan():
+def show_emergency_plan(x):
     '''
     displays the emergency plan in a frame
     also displays a search bar that searches by plan name
@@ -273,6 +273,9 @@ def show_emergency_plan():
     global plan_treeview
     global search_bar
     global search_entry
+    global emergencyplan_tab
+
+    emergencyplan_tab = x
 
     Label(emergencyplan_tab, text='Here are all your emergency plans:',
         width='30', font=('Calibri', 10)).pack()
@@ -307,50 +310,3 @@ def show_emergency_plan():
 
     delete_emergency_plan()
 
-def admin_logged_in():
-    '''
-    Displays admin hub as well has the various 
-    tabs for the different actions an admin can do
-    '''
-
-    global emergencyplan_tab
-    global manage_volunteer_tab
-    global admin_screen
-    global admin_camp_tab
-    
-
-    admin_screen = Tk()
-    admin_screen.title("Admin Hub")
-    admin_screen.geometry('820x620')
-    admin_screen.configure(bg='#F2F2F2')
-
-    Label(admin_screen,
-        text="Admin Hub:",
-        width="300", height="3",
-        font=("Calibri bold", 25),
-        bg='teal', fg='white').pack()
-
-    admin_hub_notebook = ttk.Notebook(admin_screen)
-    admin_hub_notebook.pack(expand=True)
-
-    emergencyplan_tab = Frame(admin_hub_notebook, width=600, height= 620, bg='#F2F2F2')
-    emergencyplan_tab.pack(fill='both', expand = True)
-
-    manage_volunteer_tab = Frame(admin_hub_notebook, width=500, height= 620, bg='#F2F2F2')
-    manage_volunteer_tab.pack(fill='both', expand= True)
-
-    admin_camp_tab = Frame(admin_hub_notebook, width=500, height= 620, bg='#F2F2F2')
-    admin_camp_tab.pack(fill='both', expand= True)
-
-
-
-    admin_hub_notebook.add(emergencyplan_tab, text='Emergency Plan')
-    admin_hub_notebook.add(manage_volunteer_tab, text='Manage Volunteers')
-    admin_hub_notebook.add(admin_camp_tab, text = "Manage Camps")
-    show_emergency_plan()
-
-
-    admin_screen.mainloop()
-
-if __name__ == '__main__':
-      admin_logged_in()
---
+++ admin_logged_in.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter import ttk
+import admin_hub_plan as ad
+
+def admin_logged_in():
+    '''
+    Displays admin hub as well has the various 
+    tabs for the different actions an admin can do
+    '''
+
+    global emergencyplan_tab
+    global manage_volunteer_tab
+    global admin_screen
+    global admin_camp_tab
+    
+
+    admin_screen = Tk()
+    admin_screen.title("Admin Hub")
+    admin_screen.geometry('820x620')
+    admin_screen.configure(bg='#F2F2F2')
+
+    Label(admin_screen,
+        text="Admin Hub:",
+        width="300", height="3",
+        font=("Calibri bold", 25),
+        bg='teal', fg='white').pack()
+
+    admin_hub_notebook = ttk.Notebook(admin_screen)
+    admin_hub_notebook.pack(expand=True)
+
+    emergencyplan_tab = Frame(admin_hub_notebook, width=600, height= 620, bg='#F2F2F2')
+    emergencyplan_tab.pack(fill='both', expand = True)
+
+    manage_volunteer_tab = Frame(admin_hub_notebook, width=500, height= 620, bg='#F2F2F2')
+    manage_volunteer_tab.pack(fill='both', expand= True)
+
+    admin_camp_tab = Frame(admin_hub_notebook, width=500, height= 620, bg='#F2F2F2')
+    admin_camp_tab.pack(fill='both', expand= True)
+
+    admin_hub_notebook.add(emergencyplan_tab, text='Emergency Plan')
+    admin_hub_notebook.add(manage_volunteer_tab, text='Manage Volunteers')
+    admin_hub_notebook.add(admin_camp_tab, text = "Manage Camps")
+    
+    ad.show_emergency_plan(emergencyplan_tab)
+    
+    admin_screen.mainloop()
+
+admin_logged_in()
