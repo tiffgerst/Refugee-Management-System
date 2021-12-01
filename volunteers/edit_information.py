b1ee9ae09fd2bc80cd25da5f9843be43c5266feb
tiffgerst <tiffany.gerstmeyr@web.de>
2021-12-01 15:08:42 +0000

    start

--- volunteers/edit_information.py
+++ volunteers/edit_information.py
@@ -3,8 +3,147 @@ from tkinter import ttk
 
 
 def edit_popup():
-    pass
+    global edit_details
+    global editor_popup
+    global refugee_family_name
+    global refugee_first_name
+    global num_relatives
+    global medical_conditions
+    global camp_id
+    global on_site
+    global default_first_name
 
+    editor_popup = Toplevel(refugee_tab)
+    editor_popup.title('Editor')
+    editor_popup.geometry('600x500')
+
+    editor_popup.configure(bg='#F2F2F2')
+
+    Label(editor_popup, text="Please edit the following details:",
+        width="300", height="3",
+        font=("Calibri bold", 25),
+        bg='grey', fg='white').pack()
+
+    # Define some variables to be used as inputs
+    vol_username = StringVar()
+    vol_password = StringVar()
+    vol_camp_id = StringVar()
+    vol_phonenumbermedical_conditions = StringVar()
+    vol_medic = StringVar()
+    
+    
+
+    # Event selected -> get the dictionary of values of the event
+    selected_refugee = refugee_treeview.focus()
+
+    # Set the default strings on the form using existing data of event
+    default_first_name = refugee_treeview.item(selected_refugee)['values'][0]
+    default_family_name = refugee_treeview.item(selected_refugee)['values'][1]
+    default_camp_id = refugee_treeview.item(selected_refugee)['values'][2]
+    default_medical_conditions = refugee_treeview.item(selected_refugee)['values'][3]
+    default_num_relatives = refugee_treeview.item(selected_refugee)['values'][4]
+    default_on_site = refugee_treeview.item(selected_refugee)['values'][5]
+    Label(editor_popup, text="", bg='#F2F2F2').pack()
+
+    Label(editor_popup, text='Refugee First Name: *', bg='#F2F2F2', font=("Calibri", 15)).pack()
+    refugee_first_name_label = Entry(editor_popup, textvariable=refugee_first_name, width='30', font=("Calibri", 10))
+    refugee_first_name_label.insert(END, default_first_name)
+    refugee_first_name_label.pack()
+
+    Label(editor_popup, text='Refugee Family Name: *', background='#F2F2F2', font=("Calibri", 15)).pack()
+    refugee_family_name_label = Entry(editor_popup, textvariable=refugee_family_name, width="30", font=("Calibri", 10))
+    refugee_family_name_label.insert(END, default_family_name)
+    refugee_family_name_label.pack()
+
+    Label(editor_popup, text='Camp ID: *', bg='#F2F2F2', font=("Calibri", 15)).pack()
+    camp_id_label = Entry(editor_popup, textvariable=camp_id, width="30", font=("Calibri", 10))
+    camp_id_label.insert(END, default_camp_id)
+    camp_id_label.pack()
+
+    Label(editor_popup, text='Medical Conditions: *', bg='#F2F2F2', font=("Calibri", 15)).pack()
+    medical_conditions = Entry(editor_popup, textvariable=medical_conditions, width="30", font=("Calibri", 10))
+    medical_conditions.insert(END, default_medical_conditions)
+    medical_conditions.pack()
+
+    Label(editor_popup, text='Number of Relatives: *', bg='#F2F2F2', font=("Calibri", 15)).pack()
+    num_relatives_label = Entry(editor_popup, textvariable=num_relatives, width="30", font=("Calibri", 10))
+    num_relatives_label.insert(END, default_num_relatives)
+    num_relatives_label.pack()
+
+    Button(editor_popup, text="Edit Refugee", height="2", width="30", command=edit_refugee).pack(pady=10)
+
+
+def edit_refugee():
+    """
+    Replaces the values edited by the user and adds them to the csv
+    Refreshes the treeview and gives a popup saying it was successful
+    """
+
+    global edit_success_popup
+
+    # Retrieve the variables using .get() - value is str
+    refugee_fi = refugee_first_name.get()
+    refugee_fa = refugee_family_name.get()
+    refugee_camp = camp_id.get()
+    refugee_cond = medical_conditions.get()
+    refugee_rel = num_relatives.get()
+    refugee_on = on_site.get()
+
+    # Check for blanks
+    res = check_blanks(
+        form={
+        'first_name':refugee_fi,'family_name':refugee_fa,'camp_id':refugee_camp,
+        'medical_conditions':refugee_cond,'num_relatives':refugee_rel},
+        parent=editor_popup)
+    if res == False: return
+
+    # Open csv -> change the refugee attributes -> save csv
+    df = pd.read_csv('data/refugees.csv')
+    updated_row = [refugee_fi, refugee_fa, refugee_camp, refugee_cond, refugee_rel, refugee_on]
+    df.loc[df['first_name'] == default_first_name] = [updated_row]
+    df.to_csv('data/refugees.csv',index=False)
+
+    # Clears and updates the treeview
+    clear_treeview()
+    update_treeview()
+
+    # Creates a popup that tells user the refugee edit was successful
+    edit_success_popup = Toplevel(editor_popup)
+    edit_success_popup.title("Success")
+    Label(edit_success_popup, text="Refugee edit was successful", fg='green').pack()
+    Button(edit_success_popup, text="OK", command=lambda: delete_popups([edit_success_popup,editor_popup])).pack()
+
+    edit_details = Toplevel(sign_up_screen)
+    edit_details.title("Edit your details")
+    edit_details.geometry("500x620")
+    Label(edit_details, text="Registration was successful", fg='green').pack()
+    Button(edit_details, text="OK", command=delete_register_sucess).pack()
+
+def clear_treeview():
+    """
+      Clears the table so that it can be reloaded
+    """
+
+    refugee_treeview.delete(*refugee_treeview.get_children())
+
+
+def update_treeview():
+    """
+    Tree view logic for viewing  refugee csv
+    """
+
+    #opens csv using pandas and converts columns to list
+    #also makes the first columns headings
+    df = pd.read_csv('data/refugees.csv')
+    refugee_treeview["column"] = list(df.columns)
+    refugee_treeview["show"] = "headings"
+
+    for column in refugee_treeview["column"]:
+        refugee_treeview.heading(column, text=column)
+
+    #retrieves rows and displays them
+    for _,row in df.iterrows():
+        refugee_treeview.insert("", "end", values=list(row.values))
 
 def plan_edit_window(tab):
     """
